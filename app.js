57284b104e997015789d8cdc7b2d7d44fcb26bd4
mohankrishnan-github <mohan25701@gmail.com>
2021-05-26 21:30:08 +0530

    mytodolist-v1

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+var items=["Do Meditation","Play Badmiton","Revise Old Things","Learn New Things","Do Some Coding"];
+app.get("/",function(req,res){
+  var today = new Date();
+  var option = {
+    weekday : "long",
+    day : "numeric",
+    month : "long"
+  }
+
+  thatday=today.toLocaleDateString("en-us",option);
+
+  res.render("index",{day : thatday, item : items });
+});
+app.post("/",function(req,res){
+  items.push(req.body.newList);
+  res.redirect("/");
+});
+
+
+
+app.listen(3000,function(){
+  console.log("server working fine");
+});
